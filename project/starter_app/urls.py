d869b636a04d02da10dbdaa09d93696100e46ef3
pythonfun0 <python_fun@gmail.hu>
2016-04-10 19:24:08 +0200

    Json

--- project/starter_app/urls.py
+++ project/starter_app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.content_from_json, name='content_from_json'),
+    # url(r'^$', views.home, name='home'),
 ]
--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
+import json
+import os
 # from .models import Message
 
 
-def home(request):
-    # messages = Message.objects.order_by('order')
-    messages = ['1st', '2nd', '3th']
-    context_dict = {
-        'messages': messages
+module_dir = os.path.dirname(__file__)  # get current directory
+file_path = os.path.join(module_dir, 'data.json')
+
+
+counter = 0
+relode_counter = -1
+with open(file_path) as from_json:
+        loaded_data = json.load(from_json)['new blog content']
+        counter += 1
+from_json.close()
+
+
+def content_from_json(request):
+    global relode_counter
+    relode_counter += 1
+    text = {
+        'json_data': loaded_data,
+        'loaded_num': 'The json file was loaded only at %s!' % counter,
+        'relode_counter': 'The page was reloded: %s ' % relode_counter
     }
-    return render(request, 'starter_app/home.html', context_dict)
+    return render(request, 'starter_app/home.html', text)
+
+
+# def home(request):
+#     messages = Message.objects.order_by('order')
+#     messages = ['1st', '2nd', '3th']
+#     context_dict = {
+#         'messages': messages
+#     }
+#     return render(request, 'starter_app/home.html', context_dict)
